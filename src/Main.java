1724a41c3ec3ba47e3d9a5c83009e484e581d57d
Artem Sergeev <rus21docent@gmail.com>
2023-05-22 03:15:53 +0300

    Add Math Operations

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secodNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secodNumber;
+        int dif = firstNumber - secodNumber;
+        int multiply = firstNumber * secodNumber;
+        double quotient = (double) firstNumber / secodNumber;
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + dif);
+        System.out.println("Произведение: " + multiply);
+        System.out.println("Частное: " + quotient);
+
+
     }
 }

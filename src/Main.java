5152bd199fa0a32d8139d5af332f18cb086358c6
Artem Sergeev <rus21docent@gmail.com>
2023-06-07 11:50:59 +0300

    Add search path to file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int countOfCorrectlySpecifiedPathsToFiles = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует. Или указанный путь является путём к папке" + "\n");
+                continue;
+            } else {
+                System.out.println("Путь указан верно");
+                countOfCorrectlySpecifiedPathsToFiles++;
+                System.out.println("Это файл номер " + countOfCorrectlySpecifiedPathsToFiles + "\n");
+            }
+        }
     }
 }
